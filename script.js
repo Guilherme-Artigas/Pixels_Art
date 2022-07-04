d12b10eb4ee7d5de1126de674690d72f78e32e53
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-04 16:19:21 -0300

    Requisito 9,10 resolvidos

--- script.js
+++ script.js
@@ -43,4 +43,29 @@ corblueVioletWheel.addEventListener('click', function () {
 
 ulPixelBoard.addEventListener('click', function (arg1) {
   arg1.target.className = 'pixel ' + selected;
-})
+});
+
+const buttonClear = window.document.querySelector('button#clear-board');
+buttonClear.addEventListener('click', function () {
+  const allLi = window.document.querySelectorAll('li.pixel');
+  for (let i of allLi) {
+    i.className = 'pixel white';
+  };
+});
+
+const buttonGenerator = window.document.querySelector('button#generate-board');
+buttonGenerator.addEventListener('click', function () {
+  const boardSizeString = window.document.querySelector('input#board-size');
+  if (boardSizeString.value === '') {
+    alert('Board inválido!');
+  } else {
+    const boardSizeNumber = parseInt(boardSizeString.value);
+    ulPixelBoard.style.height = '100%';
+    ulPixelBoard.style.width = '100%';
+    for (let indice = 1; indice <= (boardSizeNumber * boardSizeNumber - 25); indice += 1) {
+      const itemLista = window.document.createElement('li');
+      itemLista.className = 'pixel white';
+      ulPixelBoard.appendChild(itemLista);
+    };
+  };
+});
